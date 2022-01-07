99ac88d7a140fb7561fa37aa23b1bdeeb9d01cb0
VishalVinayRam <vishalvinayram5432@gmail.com>
2022-01-07 17:53:35 +0530

    final comiit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +27,58 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Widget> oPtion_check(option)
+  {
+    var hello;
+    var  icons_color;
+    if(option == Icons.check)
+      {
+        hello = Icons.check;
+        icons_color =Colors.green;
+
+      }
+    else
+      {
+        hello=Icons.clear;
+        icons_color=Colors.red;
+      }
+    return <Widget>[
+        Icon(
+          hello,
+          color:icons_color,
+          size:20.0,
+        ),
+    ];
+      /*  Icon(
+          hello,
+          color:icons_color,
+          size:11.0,
+        )*/
+  }
+
+  int q_no=0;
+  List<String> question = [
+    'Hello word is the first statement typed ',
+    'This is vishalv is vishal vinay ram',
+    'I am a student from uvce',
+    'This is the text for random',
+    'text 2 for random',
+
+  ];
+  bool correct;
+  void printing()
+  {
+    print("hello world");
+  }
+  List<bool> answers=[
+    true,
+    false,
+    true,
+    false,
+    true
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +91,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                question[q_no],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -51,6 +105,7 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
+             // onLongPress: oPtion_check(),
               textColor: Colors.white,
               color: Colors.green,
               child: Text(
@@ -62,6 +117,22 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                correct = answers[q_no];
+                if(correct == true )
+                  {
+                    oPtion_check(Icons.check);
+                  }
+                else
+                  {
+                    oPtion_check(Icons.clear);
+                  }
+                setState(() {
+                    {
+                      q_no = q_no+1;
+                      printing();
+                      oPtion_check(Icons.check);
+                    }
+                });
               },
             ),
           ),
@@ -78,13 +149,46 @@ class _QuizPageState extends State<QuizPage> {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
+              onPressed: () {//The user picked false.
+                correct =answers[q_no];
+                if(correct=='false')
+                  {
+                   oPtion_check(Icons.check);
+                   setState(() {
+                     oPtion_check(Icons.check);
+                   });
+                  }
+                else {
+                  oPtion_check(Icons.clear);
+                  setState(() {
+                    oPtion_check(Icons.clear);
+                  });
+                };
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        /*Row(
+          children: <Widget>[
+            Icon(
+             Icons.check,
+             color:Colors.green,
+              size:11.0,
+            ),
+            Icon(
+              Icons.clear,
+              color:Colors.red,
+              size:11.0,
+            )
+          ],
+        )*/
+        Row(
+          children: [
+            //oPtion_check(Icons.check),
+            //oPtion_check(Icons.clear),
+           ])
+
       ],
     );
   }
